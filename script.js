42f03c60081f0f847b7afd7bd9084fc192fef1b4
sid12261996 <sidharthrkc@gmail.com>
2022-07-26 08:44:59 +0530

    dynamic color for the boxes and drag and drop working cool

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addBoxes() {
         const span = document.createElement('SPAN');
         span.textContent = index;
         box.classList.add('box');
+        box.style.backgroundColor = generateRandomColor();
         box.setAttribute('draggable', true);
         box.setAttribute('position-count', index);
         box.append(span);
@@ -20,48 +21,55 @@ let iniiatorIndex = -1;
 
 boxes.forEach(box => {
     box.addEventListener('dragstart', (e) => dragAction(e, true));
+    box.addEventListener('dragenter', (e) => dragAction(e, true, 'outline'));
+    box.addEventListener('dragleave', (e) => dragAction(e, false, 'outline'));
     box.addEventListener('dragover', (e) => e.preventDefault());
+    box.addEventListener('drop', drop, true);
     box.addEventListener('dragend', (e) => dragAction(e, false));
-    box.addEventListener('drop', drop);
 });
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
 
 // container.addEventListener('dragover', drop);
 
 function drop(event) {
-    console.log('drop is successful');
+    // console.log('drop is successful');
     event.preventDefault();
     const dropedElement = event.target;
+    if (!dropedElement.classList.contains('box')) {
+        return;
+    }
+    dragAction(event, false, 'outline')
     const initiatedElement = container.querySelector('.dragging');
     dropAndSwapping(dropedElement, initiatedElement);
 }
 
-function dragAction(event, start) {
+function dragAction(event, start, className = 'dragging') {
     // console.log(event);
     if (start) {
-        event.target.classList.add('dragging');
+        event.target.classList.add(className);
     } else {
-        event.target.classList.remove('dragging');
+        event.target.classList.remove(className);
     }
 }
 
+function selectTheParent() { }
+
 function dropAndSwapping(nodeA, nodeB) {
-    // const droppedIndex = droppedElement.getAttribute('position-count');
-    // const initiatorIndex = initiatorElement.getAttribute('position-count');
-    // droppedElement.setAttribute('position-count', initiatorIndex);
-    // initiatorElement.setAttribute('position-count', droppedIndex);
-    const parentA = nodeA.parentNode;
+    const parent = container;
     const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
 
     // Move `nodeA` to before the `nodeB`
     nodeB.parentNode.insertBefore(nodeA, nodeB);
 
     // Move `nodeB` to before the sibling of `nodeA`
-    parentA.insertBefore(nodeB, siblingA);
-    // container.replaceChild(droppedElement, initiatorElement);
-    // container.insertBefore()
-    // container.replaceChild(initiatorElement, droppedElement);
-
-
-
-    // console.log('the element is dropped on ', droppedIndex, ' and initiated by ', initiatorIndex);
+    parent.insertBefore(nodeB, siblingA);
+    // console.log(nodeA, nodeB);
 }
